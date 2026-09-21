3927beb270906ed18c419ab95ea8315edfc5a527
arvindyadav9012 <chintukumar325033@gmail.com>
2026-09-21 21:59:32 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// NMRC Inventory V41.0 PORTAL configuration.
+// Keep /api when the Live Sync Worker is routed to this Pages site.
+window.NMRC_INVENTORY_API_BASE = '/api';
+window.NMRC_INVENTORY_API_KEY = '';
+window.NMRC_INVENTORY_DRIVE_BACKUP_URL = '';
+window.NMRC_INVENTORY_SYNC_POLL_MS = 1500;
